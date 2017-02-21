45652f608d6d1cf50d609a2c89ea796d618e0945
Billyboy <billy_labay@yahoo.com>
2017-02-21 21:48:32 +0800

    applicant notif update
    added notif ui and time ago
    to be added later: individual notif and overall

--- application/views/dashboard/applicant/notif-view.php
+++ application/views/dashboard/applicant/notif-view.php
@@ -5,7 +5,10 @@
                 <div>
                     <i class="fa fa-comment fa-fw"></i> <?= $notification->notifMessage ?>
                     <br>
-                    <span class="pull-left text-muted small">4 minutes ago</span>
+                    <?php
+
+                     ?>
+                    <span class="pull-left text-muted small"><?= $notification->createdAt ?></span>
                 </div>
             </a>
         </li>
