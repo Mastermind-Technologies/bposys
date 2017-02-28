d0c117e6bdaa356b02ce8ea28c1178494b8474aa
Billyboy <billy_labay@yahoo.com>
2017-02-28 14:02:22 +0800

    qqq

--- application/views/dashboard/bplo/index.php
+++ application/views/dashboard/bplo/index.php
@@ -3,7 +3,7 @@
   <div id="content-header">
     <div id="breadcrumb">
       <a href="<?php echo base_url(); ?>dashboard" class="tip-bottom"><i class="icon-home"></i> Dashboard</a>
-      <a href="#" class="current">View Reports</a>
+      <a href="#" class="current">Home</a>
     </div>
     <!--End-breadcrumbs-->
   </div>
